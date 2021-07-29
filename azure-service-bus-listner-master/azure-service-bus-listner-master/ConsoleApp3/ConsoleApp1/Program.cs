b51f1a05c5774ca2871853d384f35dc86f992a10
shebin070 <shebincbabu1994@gmail.com>
2021-07-29 07:31:47 +0530

    first commit

---
+++ azure-service-bus-listner-master/azure-service-bus-listner-master/ConsoleApp3/ConsoleApp1/Program.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static ISubscriptionClient subscriptionClient;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PaymentSucess - Listner started.");
+            MainAsync().GetAwaiter().GetResult();
+        } 
+
+        static async Task MainAsync()
+        {
+            string ServiceBusConnectionString = "Endpoint=sb://shebintest.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=dz8LSGVE7nlhgqqNOxCb0uILTq128BkA0PAGL/mW6do=";
+            string TopicName = "sales";
+            string SubscriptionName = "PaymentSucess"; // "UpdateStock";
+
+            subscriptionClient = new SubscriptionClient(ServiceBusConnectionString, TopicName, SubscriptionName);
+
+            // Register subscription message handler and receive messages in a loop.
+            RegisterOnMessageHandlerAndReceiveMessages();
+
+            Console.ReadKey();
+
+            await subscriptionClient.CloseAsync();
+        }
+
+        static void RegisterOnMessageHandlerAndReceiveMessages()
+        {
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler);
+
+            // Register the function that processes messages.
+            subscriptionClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+        }
+
+        static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            var messageBody = Encoding.UTF8.GetString(message.Body);
+
+            //var serviceBusMessage = JsonConvert.DeserializeObject<ServiceBusMessage>(messageBody);
+
+            Console.WriteLine($"Received message: UserInfo:{Encoding.UTF8.GetString(message.Body)}");
+
+            await subscriptionClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            var exception = exceptionReceivedEventArgs.Exception;
+
+            return Task.CompletedTask;
+        }
+
+        public class ServiceBusMessage
+        {
+            public string Id { get; set; }
+            public string Type { get; set; }
+            public string Content { get; set; }
+        }
+    }
+}
---
+++ azure-service-bus-send-master/azure-service-bus-send-master/user-send-microservice/user-send-microservice/Program.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace user_send_microservice
+{
+    class Program
+    {
+        static ITopicClient topicClient;
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            string ServiceBusConnectionString = "Endpoint=sb://shebintest.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=dz8LSGVE7nlhgqqNOxCb0uILTq128BkA0PAGL/mW6do=";
+            string TopicName = "sales";
+
+            topicClient = new TopicClient(ServiceBusConnectionString, TopicName);
+            string lg;
+            // Send messages.
+            do
+            {
+
+                await SendUserMessage();
+            }
+            while ((lg=Console.ReadLine()) == "1");
+
+
+            await topicClient.CloseAsync();
+        }
+
+
+        static async Task SendUserMessage()
+        {
+            List<User> users = GetDummyDataForUser();
+
+            var serializeUser = JsonConvert.SerializeObject(users);
+
+            string messageType = "userData";
+
+            string messageId = Guid.NewGuid().ToString();
+
+            var message = new ServiceBusMessage
+            {
+                Id = messageId,
+                Type = messageType,
+                Content = serializeUser
+            };
+
+            var serializeBody = JsonConvert.SerializeObject(message);
+
+            // send data to bus
+
+            var busMessage = new Message(Encoding.UTF8.GetBytes(serializeUser));
+            busMessage.UserProperties.Add("Type", messageType);
+            busMessage.MessageId = messageId;
+
+            await topicClient.SendAsync(busMessage);
+
+            Console.WriteLine("message has been sent");
+
+        }
+
+       public class User
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+        }
+
+        public class ServiceBusMessage
+        {
+            public string Id { get; set; }
+            public string Type { get; set; }
+            public string Content { get; set; }
+        }
+
+        private static List<User> GetDummyDataForUser()
+        {
+            User user = new User();
+            List<User> lstUsers = new List<User>();
+            for (int i = 1; i < 3; i++)
+            {
+                user = new User();
+                user.Id = i;
+                user.Name = "User" + i;
+
+                lstUsers.Add(user);
+            }
+
+            return lstUsers;
+        }
+    }
+}
